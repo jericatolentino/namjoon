65b62f2b6b0f2c2ae562aaa160794e771945ae65
Jerica <jericatolentino@outlook.com>
2020-09-21 10:33:49 +0100

    Fixed routing issue

--- src/browser/Landing/Landing.tsx
+++ src/browser/Landing/Landing.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import LandingCard from './LandingCard';
 
 import './Landing.css';
-import { 
-    Switch,
-    Link,
-    Route
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Landing extends React.Component<any, any> {
 
@@ -18,22 +14,12 @@ class Landing extends React.Component<any, any> {
     mainTrio = (): React.ReactFragment => {
         return (
             <React.Fragment>
-                <Link to="/Koya">KOYA</Link>
-                <Link to="/RJ">RJ</Link>
-                <Switch>
-                    <Route path="/Koya" component={() => <LandingCard title={"Koya"} text={"Koya is Joonie's mascot"}/>} />
-                    <Route path="/RJ">{this.RJRoute()}</Route>
-                </Switch>
-                <LandingCard title={"Chimmy"} text={"Chimmy is a dog"} />
-                <LandingCard title={"Kookie"} />
+                <Link to="/Koya"><LandingCard title={"Koya"} text={"Koya is Joonie's mascot"} /></Link>
+                <Link to="/Chimmy"><LandingCard title={"Chimmy"} text={"Chimmy is a dog"} /></Link>
+                <Link to="/Kookie"><LandingCard title={"Kookie"} /></Link>
             </React.Fragment>
         );
     }
-
-    RJRoute = () => {
-        return <h1>RJ</h1>;
-    }
-
 
     render() {
         return (
@@ -45,33 +31,3 @@ class Landing extends React.Component<any, any> {
 }
 
 export default Landing;
-
-/*
-          <React.Fragment>
-                <Switch>
-                    <Route path="/Koya" component={() => <LandingCard title={"Koya"} text={"Koya is Joonie's mascot"}/>} />
-                    <LandingCard title={"Chimmy"} text={"Chimmy is a dog"}/>
-                    <LandingCard title={"Kookie"}/>
-                </Switch>
-            </React.Fragment>
-
-
-             <React.Fragment>
-                <LandingCard title={"Koya"} text={"Koya is Joonie's mascot"}/>
-                <LandingCard title={"Chimmy"} text={"Chimmy is a dog"}/>
-                <LandingCard title={"Kookie"}/>
-            </React.Fragment>*/
-
-/*
-<Route path="/Koya">
-                        <LandingCard title={"Koya"} text={"Koya is Joonie's mascot"}/>
-                    </Route>*/
-
-
-/*
-   <Link to="/Koya">KOYA</Link>
-<Link to="/RJ">RJ</Link>
-<Switch>
-<Route path="/Koya" component={() => <LandingCard title={"Koya"} text={"Koya is Joonie's mascot"}/>} />
-<Route path="/RJ">{this.RJRoute()}</Route>
-</Switch>*/
---
+++ src/browser/Landing/SubLanding/Kookie.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Kookie extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="SubLanding">
+                This is Kookie's page
+            </div>
+        );
+    }
+}
+
+export default Kookie;
